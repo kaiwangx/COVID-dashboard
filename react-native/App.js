8df72090778663971e7f5c81d0d561c50e670e30
Bryan <bjin23@wisc.edu>
2020-10-22 17:53:21 -0500

    Added static data to dashboard bar graph

--- react-native/App.js
+++ react-native/App.js
@@ -7,10 +7,15 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import { VictoryBar } from 'victory-native'
 
 function HomeScreen() {
+  const data = [
+    {date: "Oct 19", cases: 7525},
+    {date: "Oct 20", cases: 4721},
+    {date: "Oct 21", cases: 4327}
+  ];
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Dashboard!</Text>
-      <VictoryBar />
+      <VictoryBar data={data} x="date" y="cases"/>
     </View>
   )
 }
