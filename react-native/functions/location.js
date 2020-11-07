7791377352ad218fb0e62ffd3d04bd232b85501e
Cannon Lock <lockcannon@hotmail.com>
2020-11-06 19:25:59 -0600

    Location Commit

--- react-native/functions/location.js
+++ react-native/functions/location.js
@@ -2,6 +2,62 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import * as Location from 'expo-location';
 
+function getStateTwoDigitCode(stateFullName) {
+  stateList = {
+    'Arizona': 'AZ',
+    'Alabama': 'AL',
+    'Alaska':'AK',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'}
+
+  return stateList[stateFullName];
+}  
+
 
 /**
  * Returns a location object of the current device
@@ -18,10 +74,7 @@ async function getZipcode( key ) {
   let location = await Location.getCurrentPositionAsync({});
   let geocode = await Location.reverseGeocodeAsync(location.coords)
 
-  return geocode[0];
+  return geocode[0][key];
 }
 
-
-
-
-export { getZipcode };
+export { getZipcode, getStateTwoDigitCode };
