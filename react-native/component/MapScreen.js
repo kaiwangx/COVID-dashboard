4dd8c46b5bf0d75389a62956b6cb90619297cbc6
Omar Zawawi <56050046+ozawawi@users.noreply.github.com>
2020-11-08 19:39:01 -0600

    Mapview & google api configurations

--- react-native/component/MapScreen.js
+++ react-native/component/MapScreen.js
@@ -1,10 +1,33 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import {PROVIDER_GOOGLE, Heatmap} from 'react-native-maps'
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Dimensions } from 'react-native';
 
 export default function MapScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Map!</Text>
+    <View style={styles.container}> 
+      <MapView  
+        showsUserLocation = {true}
+        style={styles.mapStyle} 
+        provider= {PROVIDER_GOOGLE}
+      />
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    
+  },
+});
+
+
