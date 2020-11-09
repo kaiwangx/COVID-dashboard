39666589938a998b5c707494a6497934106263a6
Kai <kwang339@wisc.edu>
2020-11-08 18:57:25 -0600

    replace react-native asyncstorage with @react-native-async-storage/async-storage

--- react-native/functions/dataCollection.js
+++ react-native/functions/dataCollection.js
@@ -1,39 +1,35 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-_retrieveData = async ( key ) => {
+_retrieveData = async (key) => {
   try {
-    const value = await AsyncStorage.getItem(key);
+    const value = await AsyncStorage.getItem(key)
     if (value !== null) {
-      return value;
+      return value
     }
   } catch (error) {
     console.log(error)
   }
-};
+}
 
-_storeData = async ( key, item ) => {
+_storeData = async (key, item) => {
   try {
-    await AsyncStorage.setItem(
-      key,
-      item
-    );
+    await AsyncStorage.setItem(key, item)
   } catch (error) {
     console.log(error)
   }
-};
+}
 
 /**
  * Assumes the array is in decreasing order
- * 
+ *
  * @param {Array} keys Array of keys to add rate of change to
  * @param {Array} data Array of Json data
  */
-function addRateOfChange( keys, data ){
-
-  var i;
-  keys.forEach( key => {
-    for ( i = 0; i < data.length - 1; i++ ){
-      data[i][key + "ROC"] = data[i][key] - data[i + 1][key];
+function addRateOfChange(keys, data) {
+  var i
+  keys.forEach((key) => {
+    for (i = 0; i < data.length - 1; i++) {
+      data[i][key + 'ROC'] = data[i][key] - data[i + 1][key]
     }
   })
 
@@ -41,93 +37,104 @@ function addRateOfChange( keys, data ){
 }
 
 /// Fields [date, deathCt, positiveCt, recoveredCt]
-async function covidCasesByZipcode( zipCode, days = 7, rateOfChange = false ){
-
-  let data;
+async function covidCasesByZipcode(zipCode, days = 7, rateOfChange = false) {
+  let data
 
   /// API rules days cannot be greater then 7
-  days = days > 7 ? 7 : days;
-  
+  days = days > 7 ? 7 : days
+
   /// If the information is up to date
-  let lastUpdateStr = await _retrieveData("lastUpdate");
-  let currentDate = new Date().setHours(0,0,0,0);
-  let lastUpdate = new Date(lastUpdateStr).setHours(0,0,0,0);
+  let lastUpdateStr = await _retrieveData('lastUpdate')
+  let currentDate = new Date().setHours(0, 0, 0, 0)
+  let lastUpdate = new Date(lastUpdateStr).setHours(0, 0, 0, 0)
 
-  if( currentDate == lastUpdate ){
+  if (currentDate == lastUpdate) {
+    /// If the information is cached
+    let dataByState = await _retrieveData('CovidCasesByZipcode')
 
-      /// If the information is cached
-      let dataByState = await _retrieveData("CovidCasesByZipcode");
-      
-      data = JSON.parse(dataByState);
+    data = JSON.parse(dataByState)
 
     /// If information is old
   } else {
-
     try {
-      let response = await fetch("https://localcoviddata.com/covid19/v1/cases/newYorkTimes?zipCode=" + zipCode + "&daysInPast=7")
-      
-      let jsonData = await response.json();
+      let response = await fetch(
+        'https://localcoviddata.com/covid19/v1/cases/newYorkTimes?zipCode=' +
+          zipCode +
+          '&daysInPast=7'
+      )
+
+      let jsonData = await response.json()
       let jsonArray = jsonData.counties[0].historicData
-      
+
       /// Update cache
-      _storeData("lastUpdate", Date());
-      _storeData("CovidCasesByZipcode", JSON.stringify(jsonArray));
+      _storeData('lastUpdate', Date())
+      _storeData('CovidCasesByZipcode', JSON.stringify(jsonArray))
 
-      data =  jsonArray;
+      data = jsonArray
+    } catch (error) {
+      console.error(error)
     }
-    catch ( error ) {
-      console.error( error );
-    }
-  }  
+  }
 
-  if( rateOfChange ){
-    let arrayLength = days < data.length ? days : data.length - 1;
+  if (rateOfChange) {
+    let arrayLength = days < data.length ? days : data.length - 1
 
-    return addRateOfChange(["deathCt", "positiveCt"], data).slice(0, arrayLength)
+    return addRateOfChange(['deathCt', 'positiveCt'], data).slice(
+      0,
+      arrayLength
+    )
   } else {
     return data
   }
-
 }
 
 /// Fields under historic value for single state
-async function covidCasesByState( state, days = 31 ){
-
-  let data;
+async function covidCasesByState(state, days = 31) {
+  let data
 
   /// If the information is up to date
-  let lastUpdateStr = await _retrieveData("lastUpdate");
-  let currentDate = new Date().setHours(0,0,0,0);
-  let lastUpdate = new Date(lastUpdateStr).setHours(0,0,0,0);
+  let lastUpdateStr = await _retrieveData('lastUpdate')
+  let currentDate = new Date().setHours(0, 0, 0, 0)
+  let lastUpdate = new Date(lastUpdateStr).setHours(0, 0, 0, 0)
 
-  if( currentDate == lastUpdate ){
+  if (currentDate == lastUpdate) {
+    let dataByState = await _retrieveData('CovidCasesByState')
 
-    let dataByState =  await _retrieveData("CovidCasesByState");
-
-    data = JSON.parse(dataByState);
-
+    data = JSON.parse(dataByState)
   } else {
     try {
+      let response = await fetch(
+        'https://api.covidtracking.com/v1/states/' + state + '/daily.json'
+      )
 
-      let response = await fetch("https://api.covidtracking.com/v1/states/" + state + "/daily.json");
+      let jsonData = await response.json()
 
-      let jsonData = await response.json();
+      _storeData('lastUpdate', Date())
+      _storeData('CovidCasesByState', JSON.stringify(jsonData))
 
-      _storeData("lastUpdate", Date());
-      _storeData("CovidCasesByState", JSON.stringify(jsonData));
-  
-      data = jsonData;
-
-    } catch ( error ) {
-      console.error(error);
-    }   
+      data = jsonData
+    } catch (error) {
+      console.error(error)
+    }
   }
 
-  return addRateOfChange(["positive", "probableCases", "negative", "pending", "totalTestResults",
-                          "hospitalizedCurrently", "hospitalizedCumulative", "inIcuCurrently",
-                          "inIcuCumulative", "recovered", "death", "hospitalized"], 
-                          data.slice(0, days + 1)).slice(0, days)
-
+  return addRateOfChange(
+    [
+      'positive',
+      'probableCases',
+      'negative',
+      'pending',
+      'totalTestResults',
+      'hospitalizedCurrently',
+      'hospitalizedCumulative',
+      'inIcuCurrently',
+      'inIcuCumulative',
+      'recovered',
+      'death',
+      'hospitalized',
+    ],
+    data.slice(0, days + 1)
+  ).slice(0, days)
 }
 
 export { covidCasesByZipcode, covidCasesByState }
