31fcf7a46d54b7a0d00ef5921e64f7e032c49f6d
Cannon Lock <lockcannon@hotmail.com>
2020-12-02 19:38:02 -0600

    Header Commit

--- react-native/component/AppHeader.js
+++ react-native/component/AppHeader.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, View, ScrollView } from 'react-native'
 
 const headerStyle = {
-  marginTop: 10,
+  marginTop: 14,
   fontSize: 34,
   textAlign: 'center',
   color: '#ff6347',
